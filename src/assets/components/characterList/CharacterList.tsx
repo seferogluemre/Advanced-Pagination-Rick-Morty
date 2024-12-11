f0083ccdbd282ff6f248b507de66f5dd8cabf19b
xEmre1-53 <157003672+xEmre1-53@users.noreply.github.com>
2024-12-11 19:05:11 +0300

    apideki pages parametresini ekleyip pagination geçişlerine göre karakter listeleme

--- src/assets/components/characterList/CharacterList.tsx
+++ src/assets/components/characterList/CharacterList.tsx
@@ -2,11 +2,17 @@ import { Alert, Button, Container, Row } from "react-bootstrap";
 import useFetch from "../../hooks/useFetch";
 import CharactersProps from "../types/Characters";
 import Card from "../layout/CharacterCard/CharacterCard";
+import { useState } from "react";
 
 const CharacterList = () => {
+  const [currentPage, setCurrentPage] = useState<number>(1);
   const { data, error, loading, pageCount } = useFetch<{
     results: CharactersProps[];
-  }>("https://rickandmortyapi.com/api/character");
+  }>(`https://rickandmortyapi.com/api/character?page=${currentPage}`);
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+  };
 
   if (error) {
     return (
@@ -40,7 +46,11 @@ const CharacterList = () => {
         </Row>
         <Row className="d-flex justify-content-center flex-row">
           {Array.from({ length: pageCount }, (_, index) => (
-            <Button key={index + 1} className="mx-1">
+            <Button
+              key={index + 1}
+              onClick={() => handlePageChange(index + 1)}
+              className="mx-1"
+            >
               {index + 1}
             </Button>
           ))}
--- src/assets/components/layout/CharacterCard/CharacterCard.tsx
+++ src/assets/components/layout/CharacterCard/CharacterCard.tsx
@@ -1,71 +1,75 @@
-import CharactersProps from '../../types/Characters';
-import './CharacterCard.scss';
+import CharactersProps from "../../types/Characters";
+import "./CharacterCard.scss";
 
-const CharacterCard = ({ name, status, gender, image, location }: CharactersProps) => {
+const CharacterCard = ({
+  name,
+  status,
+  gender,
+  image,
+  location,
+}: CharactersProps) => {
+  let statusText = "";
+  let genderText = "";
+  let statusClass = "";
 
-    let statusText = '';
-    let genderText = "";
-    let statusClass = "";
+  // Switch ifadesini burada kullanarak statusText değerini belirleyelim
+  switch (status) {
+    case "Alive":
+      statusText = "Yaşıyor";
+      statusClass = "h-2 status-color rounded-full bg-success";
+      break;
+    case "Dead":
+      statusText = "Öldü";
+      statusClass = "h-2 status-color rounded-full bg-danger";
+      break;
+    case "Unknown":
+      statusText = "Bilinmiyor";
+      statusClass = "h-2 status-color rounded-full bg-body-secondary";
+      break;
+    default:
+      statusText = "Not Status";
+      statusClass = "h-2 status-color rounded-full bg-body-tertiary";
+      break;
+  }
 
-    // Switch ifadesini burada kullanarak statusText değerini belirleyelim
-    switch (status) {
-        case 'Alive':
-            statusText = 'Yaşıyor';
-            statusClass = "h-2 status-color rounded-full bg-success"
-            break;
-        case 'Dead':
-            statusText = 'Öldü';
-            statusClass = "h-2 status-color rounded-full bg-danger"
-            break;
-        case 'Unknown':
-            statusText = 'Bilinmiyor';
-            statusClass = "h-2 status-color rounded-full bg-body-secondary"
-            break;
-        default:
-            statusText = 'Not Status';
-            statusClass = "h-2 status-color rounded-full bg-body-tertiary"
-            break;
+  const genderCondition = () => {
+    switch (gender) {
+      case "Male":
+        genderText = "Erkek";
+        return 0;
+      case "Female":
+        genderText = "Kadın";
+        return 0;
+
+      case "genderless":
+        genderText = "Cinsiyetsiz";
+        return 0;
+      default:
+        genderText = "Bilinmiyor";
+        return 0;
     }
+  };
+  genderCondition();
 
-    const genderCondition = () => {
-        switch (gender) {
-            case "Male":
-                genderText = "Erkek";
-                return 0;
-            case "Female":
-                genderText = "Kadın";
-                return 0;
-
-            case "genderless":
-                genderText = "Cinsiyetsiz";
-                return 0;
-            default:
-                genderText = "Bilinmiyor";
-                return 0;
-        }
-    }
-    genderCondition();
-
-
-    return (
-        <div className='card'>
-            <div className='card-header'>
-                <img src={image} className='img-fluid character-image' alt={name} />
-            </div>
-            <div className='card-body d-flex flex-column row-gap-2'>
-                <h6 className='character-name'>{name}</h6>
-                <div className='d-flex align-items-center column-gap-1'>
-                    <span className={statusClass}></span>
-                    <span>{statusText}</span>
-                </div>
-                <h6 className='character-gender'>{genderText}</h6>
-                <div>
-                    <p className='m-0'>Son Konum;</p>
-                    <h6 className='character-location'>{location.name}</h6>
-                </div>
-            </div>
+  return (
+    <div className="card">
+      <div className="card-header">
+        <img src={image} className="img-fluid character-image" alt={name} />
+      </div>
+      <div className="card-body d-flex flex-column row-gap-2">
+        <h6 className="character-name">{name}</h6>
+        <div className="d-flex align-items-center column-gap-1">
+          <span className={statusClass}></span>
+          <span>{statusText}</span>
         </div>
-    );
+        <h6 className="character-gender">{genderText}</h6>
+        <div>
+          <p className="m-0">Son Konum;</p>
+          <h6 className="character-location">{location.name}</h6>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CharacterCard;
